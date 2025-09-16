c403a49b4ffa3b66a6038abec367d9e217a658f5
JonathanKudsk <jonathankudsk@gmail.com>
2025-09-16 11:07:13 +0200

    så er DirectorDTO færdig

---
+++ src/main/java/app/dtos/DirectorDTO.java
@@ -0,0 +1,38 @@
+package app.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class DirectorDTO {
+
+    @JsonProperty("crew")
+    private List<Director> directors;
+
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Director {
+
+        private int id;
+
+        @JsonProperty("name")
+        private String actorsName;
+
+        @JsonProperty("character")
+        private String characterName;
+
+        private double popularity;
+
+        @JsonProperty("department")
+        private String department;
+
+        @JsonProperty("job")
+        private String jobTitle;
+
+    }
+}
